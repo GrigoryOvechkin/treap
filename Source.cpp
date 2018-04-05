04916e0a22fc1be8c93be96f563edb1d0b41e3b7
Grigory <grin98.ya@yandex.ru>
2018-04-05 20:46:37 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+
+using namespace std;
+
+#define mp make_pair
+
+struct Node
+{
+	Node *left, *right;
+	long long value, cnt, y;
+	Node() {}
+	Node(long long value)
+	{
+		this->value = value;
+		left = right = NULL;
+		y = (rand() << 16) ^ rand();
+		cnt = 1;
+	}
+};
+
+Node* nulltree = NULL;
+
+int cnt(Node* tree) //подсчет вершин в дереве
+{
+	if (tree == NULL)
+		return 0;
+	return tree->cnt;
+}
+
+int index(Node* tree)
+{
+	if (tree == NULL)
+		return -1;
+	return cnt(tree->left);
+}
+
+void update (Node* tree) //обновление информации о вершине через ее потомков 
+{
+	tree->cnt = cnt(tree->left) + cnt(tree->right) + 1;
+}
+
+Node* merge (Node* left_tree, Node* right_tree) //слияние двух деревьев
+{
+	if (left_tree == NULL)
+		return right_tree;
+	if (right_tree == NULL)
+		return left_tree;
+	if (left_tree->y > right_tree->y)
+	{
+		left_tree->right = merge(left_tree->right, right_tree);
+		update(left_tree);
+		return left_tree;
+	}
+	right_tree->left = merge(left_tree, right_tree->left);
+	update(right_tree);
+	return right_tree;
+}
+
+pair<Node*, Node*> split (Node* tree, int num) //разделение дерева по номеру вершины
+{                                              //данный номер будет последним в левом дереве
+	if (tree == NULL)                          //нумерация с 0
+		return mp(nulltree, nulltree);
+	if (index(tree) <= num)
+	{
+		auto tt = split(tree->right, num - index(tree) - 1);
+		tree->right = tt.first;
+		return mp(tree, tt.second);
+	}
+	auto tt = split(tree->left, num);
+	tree->left = tt.second;
+	return mp(tt.first, tree);
+}
+
+Node* insert (Node* tree, int value, int num) //вставка элемента по значению и номеру
+{                                             //если такого номера нет, элемент будет вставлен в ближайший возможный
+	auto tt = split(tree, num - 1);
+	Node* new_elem = new Node(value);
+	tt.first = merge(tt.first, new_elem);
+	tree = merge(tt.first, tt.second);
+	return tree;
+}
+
+Node* erase (Node* tree, int num) //удаление элемента по номеру
+{                                 //если такого номера нет, ничего не удалится
+	auto tt = split(tree, num);
+	auto tt2 = split(tt.first, num - 1);
+	tree = merge(tt2.first, tt.second);
+	return tree;
+}
+
+void print_tree (Node* tree) //вывод дерева в строчку
+{
+	if (tree == NULL)
+		return;
+	print_tree(tree->left);
+	cout << tree->value << ' ';
+	print_tree(tree->right);
+}
+
+int main()
+{
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+
+	Node* my_tree = NULL;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		int val, num;
+		cin >> val >> num;
+		my_tree = insert(my_tree, val, num);
+		print_tree(my_tree);
+		cout << "\n";
+	}
+	for (int i = 0; i < n - 5; ++i)
+	{
+		my_tree = erase(my_tree, 0);
+		print_tree(my_tree);
+		cout << "\n";
+	}
+	
+	my_tree = erase(my_tree, -1);
+	print_tree(my_tree);
+	cout << "\n";
+	my_tree = erase(my_tree, 3);
+	print_tree(my_tree);
+	cout << "\n";
+	
+	return 0;
+}
